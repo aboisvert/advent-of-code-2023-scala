88e020f133b71a673d7d89d7eab34f058aef3ab7
Alex Boisvert <alex.boisvert@gmail.com>
2023-12-04 15:58:16 -0800

    Cleanup & comments

--- day04/part1.scala
+++ day04/part1.scala
@@ -9,18 +9,19 @@ package day04
     .sum
   println(points)
 
-case class Scratchcard(id: Int, winning: Set[Int], mine: Set[Int]):
-  def points = math.pow(2, mine.count(winning) - 1).toInt
+case class Scratchcard(id: Int, winningNumbers: Set[Int], myNumbers: Set[Int]):
+  /** Returns points for this scratcard, which is 1 for 1 winning number, 2 for 2, 4 for 3, etc. */
+  def points = math.pow(2, myNumbers.count(winningNumbers) - 1).toInt
 end Scratchcard
 
-// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
 def parseScratchcard(line: String): Scratchcard =
+  // Format: "Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53"
   val Array(cardId, numbers) = line.split(""":\s+""")
   val Array(_, id) = cardId.split("""\s+""")
   val Array(winningNumbers, myNumbers) = numbers.split("""\s+\|\s+""")
   def parseNumbers(s: String) = s.split("""\s+""").map(_.toInt).toSet
   Scratchcard(
     id.toInt,
-    winning = parseNumbers(winningNumbers),
-    mine = parseNumbers(myNumbers)
+    parseNumbers(winningNumbers),
+    parseNumbers(myNumbers)
   )
--- day04/part2.scala
+++ day04/part2.scala
@@ -1,26 +1,41 @@
 package day04
 
 @main def part2 =
+  /** Accumulator */
   case class Acc(sum: Int, instances: CardInstances)
+
   val result = scala.io.Source
     .fromFile("day04/input.txt")
     .getLines
     .map(parseScratchcard)
-    .foldLeft(Acc(0, CardInstances())): (acc, card) =>
+    .foldLeft(Acc(0, new CardInstances)): (acc, card) =>
       import acc.{sum, instances}
-      println(s"card ${card.id} instances ${instances(card.id)} winningCards ${card.winningCards} ")
+
+      // Increment count of instances of following cards based on `winningCards` */
       for nextCard <- (card.id + 1) to (card.id + card.winningCards) do
         instances.increment(nextCard, instances(card.id))
-      Acc(sum + instances.dropAndGetValue(card.id), instances)
+
+      // Carry over the accumulator for `sum` and card `instances`
+      Acc(sum + instances.getValueAndDiscard(card.id), instances)
+
   println(result.sum)
 
 class CardInstances:
+  /** CardId -> count of instances */
   private val instancesMap = scala.collection.mutable.Map[Int, Int]()
+
+  /** Get count of instances for `cardId`, with a default of 1 */
   def apply(cardId: Int) = instancesMap.getOrElse(cardId, 1)
-  def increment(cardId: Int, x: Int) = instancesMap(cardId) = apply(cardId) + x
-  def dropAndGetValue(cardId: Int) = instancesMap.remove(cardId).getOrElse(1)
-  def sum = instancesMap.values.sum
+
+  /** Increment the count of `cardId` */
+  def increment(cardId: Int, inc: Int) = instancesMap(cardId) =
+    apply(cardId) + inc
+
+  /** Get the value of `cardId` then discard its value. */
+  def getValueAndDiscard(cardId: Int) = instancesMap.remove(cardId).getOrElse(1)
+
 end CardInstances
 
 extension (c: Scratchcard)
-  def winningCards = c.mine.count(c.winning)
+  /** Returns the count of winningCards */
+  def winningCards = c.myNumbers.count(c.winningNumbers)

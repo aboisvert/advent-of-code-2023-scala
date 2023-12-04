ca015638a7eb414515f08208ddd94a8d3021003c
Alex Boisvert <alex.boisvert@gmail.com>
2023-12-04 11:07:35 -0800

    day04 part2 - avoid side-effecting map() and use a fold() instead

--- day04/part2.scala
+++ day04/part2.scala
@@ -1,27 +1,25 @@
 package day04
 
-import scala.collection.mutable
-
 @main def part2 =
-  val instances = CardInstances()
-  val totalCards = scala.io.Source
+  case class Acc(sum: Int, instances: CardInstances)
+  val result = scala.io.Source
     .fromFile("day04/input.txt")
     .getLines
     .map(parseScratchcard)
-    .map: card =>
+    .foldLeft(Acc(0, CardInstances())): (acc, card) =>
+      import acc.{sum, instances}
       println(s"card ${card.id} instances ${instances(card.id)} winningCards ${card.winningCards} ")
       for nextCard <- (card.id + 1) to (card.id + card.winningCards) do
         instances.increment(nextCard, instances(card.id))
-      card
-    .map: card =>
-      instances(card.id)
-    .sum
-  println(totalCards)
+      Acc(sum + instances.dropAndGetValue(card.id), instances)
+  println(result.sum)
 
 class CardInstances:
-  private val instancesMap = mutable.Map[Int, Int]()
+  private val instancesMap = scala.collection.mutable.Map[Int, Int]()
   def apply(cardId: Int) = instancesMap.getOrElse(cardId, 1)
   def increment(cardId: Int, x: Int) = instancesMap(cardId) = apply(cardId) + x
+  def dropAndGetValue(cardId: Int) = instancesMap.remove(cardId).getOrElse(1)
+  def sum = instancesMap.values.sum
 end CardInstances
 
 extension (c: Scratchcard)

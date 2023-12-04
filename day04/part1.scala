055b6dafdefd2d84afc2da7f7511a56b470cb2ac
Alex Boisvert <alex.boisvert@gmail.com>
2023-12-04 06:58:09 -0800

    day04 part1

---
+++ day04/part1.scala
@@ -0,0 +1,24 @@
+package day04
+
+@main def part1 =
+  val points = scala.io.Source
+    .fromFile("day04/input.txt")
+    .getLines
+    .map(parseScratchcard)
+    .map(_.points)
+    .sum
+  println(points)
+
+case class Scratchcard(winning: Set[Int], mine: Set[Int]):
+  def points = math.pow(2, mine.count(winning) - 1).toInt
+end Scratchcard
+
+// Card 1: 41 48 83 86 17 | 83 86  6 31 17  9 48 53
+def parseScratchcard(line: String): Scratchcard =
+  val Array(cardId, numbers) = line.split(""":\s+""")
+  val Array(winningNumbers, myNumbers) = numbers.split("""\s+\|\s+""")
+  def parseNumbers(s: String) = s.split("""\s+""").map(_.toInt).toSet
+  Scratchcard(
+    winning = parseNumbers(winningNumbers),
+    mine = parseNumbers(myNumbers)
+  )
